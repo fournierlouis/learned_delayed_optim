bd0e66490136a4a705c8696d6ec8cf73dc50107e
Louis Fournier <louis.fournier12@gmail.com>
2024-02-15 00:50:38 +0100

    IT LIVES launch morefeat

---
+++ src/launch_meta_train_mlp_morefeat.sh
@@ -0,0 +1,6 @@
+sbatch meta_train_mlp_morefeat.slurm 1
+sbatch meta_train_mlp_morefeat.slurm 2
+sbatch meta_train_mlp_morefeat.slurm 4
+sbatch meta_train_mlp_morefeat.slurm 8
+sbatch meta_train_mlp_morefeat.slurm 16
+sbatch meta_train_mlp_morefeat.slurm
